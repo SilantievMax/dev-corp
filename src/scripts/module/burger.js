d23d42779166b411956bedbe843ecba9364031d8
SilantievMax <nohrin.max@yandex.ru>
2022-10-04 01:48:05 +0300

    modified components header and footer

--- src/scripts/module/burger.js
+++ src/scripts/module/burger.js
@@ -1,4 +1,6 @@
 const nav = document.querySelector('.header__burger');
+const navList = document.querySelector('.header__nav')
 nav.addEventListener('click',(event) => {
     nav.classList.toggle('open');
+    navList.classList.toggle('header__nav_active')
 });
