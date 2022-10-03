66e3313b40aec7cc843e2239161b38d725299803
SilantievMax <nohrin.max@yandex.ru>
2022-10-03 22:17:17 +0300

    added component swiperFounders

--- src/scripts/module/swiper.js
+++ src/scripts/module/swiper.js
@@ -1,16 +1,42 @@
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: true,
-    },
-    pagination: {
-        el: ".swiper-pagination",
-    },
-});
+const slider = document.querySelector(".swiper-founders__items");
+const slides = document.querySelectorAll(".swiper-founders__item");
+const button = document.querySelectorAll(".swiper-founders__button");
+
+let current = 0;
+let prev = 4;
+let next = 1;
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener("click", () =>
+        i == 0 ? gotoPrev() : gotoNext()
+    );
+}
+
+const gotoPrev = () =>
+    current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
+
+const gotoNext = () => (current < 3 ? gotoNum(current + 1) : gotoNum(0));
+
+const gotoNum = (number) => {
+    current = number;
+    prev = current - 1;
+    next = current + 1;
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+        slides[i].classList.remove("prev");
+        slides[i].classList.remove("next");
+    }
+
+    if (next == 4) {
+        next = 0;
+    }
+
+    if (prev == -1) {
+        prev = 3;
+    }
+
+    slides[current].classList.add("active");
+    slides[prev].classList.add("prev");
+    slides[next].classList.add("next");
+};
